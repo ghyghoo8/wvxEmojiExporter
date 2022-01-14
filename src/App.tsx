5b7f3d3a8621b7b3c4fa82f589ecf2efc0e6237c
yefanglin <yefanglin@joyy.com>
2022-01-14 16:49:05 +0800

    add message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-23 20:14:22
- * @LastEditTime: 2022-01-13 16:06:52
+ * @LastEditTime: 2022-01-14 16:48:24
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /scaffold-demo/tauri-app/tauri-app/src/App.tsx
@@ -33,6 +33,7 @@ const App: FC = () => {
     const folderPath = `${path.replace(JSON_FILE_NAME, '')}${code}/Caches/Emotions`
     if (code) {
       setAppFolderPath(folderPath);
+      message.success('导出工作准备完成');
     } else {
       message.error('未找到应用地址');
     }
